ac0df3ee4ed77066805d668539700ee837524b2a
Elliot <elliotchan120@gmail.com>
2026-05-06 19:28:55 -0400

    small edits to goals

--- src/modules/nano_pipeline/drug_encoder.py
+++ src/modules/nano_pipeline/drug_encoder.py
@@ -1,2 +1,5 @@
 from src.utility.data_modules import *
 from src.utility.path_modules import *
+
+# drug tokenizing: molecular fingerprint
+# note: input smiles should be screened first for drug-likeness beforehand
--- src/modules/nano_pipeline/protein_encoder.py
+++ src/modules/nano_pipeline/protein_encoder.py
@@ -1,2 +1,4 @@
 from src.utility.data_modules import *
 from src.utility.path_modules import *
+
+# protein tokenizing: 1 token = spatial data + physicochemical data
