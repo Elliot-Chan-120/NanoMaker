27e2fa2b28d5b8a60c4542007d8a84de608a7398
Elliot <elliotchan120@gmail.com>
2026-06-02 02:47:53 -0400

    data notebooks are now actual notebooks, with finalized versions of modules saved separately. made two separate pointer sources with the same smiles split (random seed) so that naanobot can train properly without void tokens messing up coordinate saving + generation. now moving on to custom loss function for naanobot.

--- src/nano_maker/modules/nAAno/naanoeng.py
+++ src/nano_maker/modules/nAAno/naanoeng.py
@@ -1,31 +1,31 @@
 # protein feature engineering:
 # spatial data + physicochemical data
-# when you pass an AA seq through ESM2, you get back a matrix of shape:
-# [sequence length x embedding dimension] - big problem
-# each row is a per-residue embedding, a vector that encodes that AA's...
-# - identity, local chemical environment, inferred structural context as a vector
 # add parts of GEM to append a physiochemical vector on top of spatial one
 # - charge, hydrophobicity, pKa, H-bond capacity, .etc
 # GEM properties = nAAno_token
 
-# I don't think we need a module for tokenizing, this isn't NLP
+# feature vectors as "tokens" per amino acid
 # put stuff in nAAno_library for physicochemical analysis
 
-# TODO: ramachandran freedom feature
-
+import torch
 from src.nano_maker.modules.nAAno.naanolibrary import *
 
 class NAAnoEng:
     """Run this everytime we need a new set of feature vectors"""
-    def __init__(self, verbose=False):
+    def __init__(self, max_angstroms, block_size, verbose=False):
+        self.max_angstroms = max_angstroms
+        self.block_size = block_size
         self.verbose = verbose
 
     def initialize(self):
-        self.nAAno_vectors = {aa_id: build_nAAnoVector(aa_id) for aa_id in AA_IDS}
+        self.nAAno_vectors = {aa_id: self.build_nAAnoVector(aa_id) for aa_id in AA_IDS}
 
         if self.verbose:
             print("NAAnoEng initialized")
         return True
+
+    def n_features(self):
+        return len(self.nAAno_vectors['A'])
 
     def get_aa_id(self, naano_vector):
         aa_id = None
@@ -42,36 +42,90 @@ class NAAnoEng:
     def get_nAAnovector(self, aa_id):
         return self.nAAno_vectors[aa_id]
 
+    @staticmethod
+    def build_nAAnoVector(aa_id: str):
+        """
+        use this in these two scenarios:
+        \n    - generating embeddings after updating feature vector
+        \n    - inference
+        :param aa_id: single letter amino acid reference code
+        :returns: feature vector representing a given (valid) amino acid
+        """
+        # sanity check
+        if aa_id not in AA_IDS:
+            raise ValueError(f"{aa_id} not in valid AA ID list")
 
-def build_nAAnoVector(aa_id: str):
-    """
-    use this in these two scenarios:
-    \n    - generating embeddings after updating feature vector
-    \n    - inference
-    :param aa_id: single letter amino acid reference code
-    :returns: feature vector representing a given (valid) amino acid
-    """
-    # sanity check
-    if aa_id not in AA_IDS:
-        raise ValueError(f"{aa_id} not in valid AA ID list")
+        # embedding scheme, MAKE SURE TO UPDATE THIS IF YOU EVER UPDATE NAANOLIBRARY
+        naano_vector = [
+            MOLECULAR_WEIGHTS[aa_id],
+            NET_CHARGES[aa_id],
+            ISOELECTRIC_PTS[aa_id],
+            HYDROPHOBICITY_IDXS[aa_id],
+            HALF_LIFE[aa_id],
+        ]
+        naano_vector += FUNCTIONAL_FP[aa_id]
+        naano_vector += PROPENSITIES[aa_id]
+        return naano_vector
 
-    # embedding scheme, MAKE SURE TO UPDATE THIS IF YOU EVER UPDATE NAANOLIBRARY
-    naano_vector = [
-        MOLECULAR_WEIGHTS[aa_id],
-        NET_CHARGES[aa_id],
-        ISOELECTRIC_PTS[aa_id],
-        HYDROPHOBICITY_IDXS[aa_id],
-        HALF_LIFE[aa_id],
-    ]
-    naano_vector += FUNCTIONAL_FP[aa_id]
-    return naano_vector
+    # generation + training data processing
+    def get_nAAno_X(self, coord_context, bioch_context, coord_Y):
+        # go through coordinate context (iterate through range)
+        # calculate and add relative coordinates concat. with nAAnovectors
+        naano_X = []
+        coord_Y = torch.tensor(coord_Y, dtype=torch.float32)
 
+        for idx in range(self.block_size):
+            coord_X = torch.tensor(coord_context[idx], dtype=torch.float32)
+            naanovector_X = torch.tensor(bioch_context[idx])
+            # construct augmented relative coordinate vector then concat with nAAno_token
+            # XYZ
+            relative_vect = self.sph_to_xyz(coord_X) - self.sph_to_xyz(coord_Y)  # 3
+            euclidean_dist = (torch.norm(relative_vect) + 1e-8).unsqueeze(0)   # 1
+            unit_dir = (relative_vect / euclidean_dist)   # 3
+
+            # spherical
+            Xr, Xaz, Xpl = coord_X
+            Yr, Yaz, Ypl = coord_Y
+            r_diff = Yr - Xr.unsqueeze(0)    # 1
+            az_diff = self.angle_diff(Xaz, Yaz).unsqueeze(0)   # 1
+            pl_diff = self.angle_diff(Xpl, Ypl).unsqueeze(0)  # 1
+
+            naano_X.append(torch.cat([naanovector_X, relative_vect, euclidean_dist, unit_dir,
+                                        r_diff, az_diff, pl_diff]))
+
+        return torch.stack(naano_X)  # block_size, 32
+
+    def approx_id(self, vector):
+        min_error = None
+        approximate_identity = None
+        for aa_id, n_v in self.nAAno_vectors.items():
+            error = vector - n_v
+            if error < min_error:
+                min_error = error
+                approximate_identity = aa_id
+
+        return approximate_identity
+
+
+    @staticmethod
+    def sph_to_xyz(spherical_vector):
+        v = torch.tensor(spherical_vector, dtype=torch.float32)
+        r, az, pl = v[0], v[1], v[2]
+        x = r * torch.sin(pl) * torch.cos(az)
+        y = r * torch.sin(pl) * torch.sin(az)
+        z = r * torch.cos(pl)
+        return torch.stack([x, y, z])
+
+    @staticmethod
+    def angle_diff(aX, aY):
+        return ((torch.cos(aX) - torch.cos(aY))**2 + (torch.sin(aX) - torch.sin(aY))**2).mean()
 
 def encoder_check(verbose=True):
-    module = NAAnoEng(verbose=True)
+    module = NAAnoEng(max_angstroms=42, block_size=42, verbose=False)
     module.initialize()
     for aa_code, aa_vect in module.nAAno_vectors.items():
         print(f"{aa_code} -- {aa_vect}")
+    print(f"{module.n_features()} features")
 
     # check decoder and encoder
     for aa in AA_IDS:
--- src/nano_maker/modules/nAAno/naanolibrary.py
+++ src/nano_maker/modules/nAAno/naanolibrary.py
@@ -24,6 +24,7 @@ MOLECULAR_WEIGHTS = {
     'V': 0.12,
     'W': 0.20,
     'Y': 0.18,
+    'VOID': -0.1
 }
 
 # net charges at physiological pH ~7.4
@@ -48,6 +49,7 @@ NET_CHARGES = {
     'V': 0,
     'W': 0,
     'Y': 0,
+    'VOID': 0
 }
 
 # isoelectric point pHs from peptide web
@@ -73,6 +75,7 @@ ISOELECTRIC_PTS = {
     'V': 5.97,
     'W': 5.94,
     'Y': 5.66,
+    'VOID': 0.00,
 }
 
 # hydrophobicity levels - pH 7
@@ -80,52 +83,56 @@ ISOELECTRIC_PTS = {
 # around 0 = neutral
 # -ve values = hydrophilic
 HYDROPHOBICITY_IDXS = {
-    'A': 41,
-    'C': 49,
-    'D': -55,
-    'E': -31,
-    'F': 100,
-    'G': 0,
-    'H': 8,
-    'I': 100,
-    'K': -23,
-    'L': 97,
-    'M': 74,
-    'N': -28,
-    'P': -46,
-    'Q': -10,
-    'R': -14,
-    'S': -5,
-    'T': 13,
-    'V': 76,
-    'W': 97,
-    'Y': 63,
-}
-
-
-# individual half lives
-HALF_LIFE = {
-    'A': 4.4,
-    'C': 1.2,
-    'D': 1.1,
-    'E': 1,
+    'A': 0.41,
+    'C': 0.49,
+    'D': -0.55,
+    'E': -0.31,
     'F': 1,
-    'G': 30,
-    'H': 3.5,
-    'I': 20,
-    'K': 1.3,
-    'L': 5.5,
-    'M': 30,
-    'N': 1.4,
-    'P': 21,
-    'Q': 0.8,
-    'R': 1,
-    'S': 1.9,
-    'T': 7.2,
-    'V': 100,
-    'W': 2.8,
-    'Y': 2.8,
+    'G': 0.00,
+    'H': 0.08,
+    'I': 1.00,
+    'K': -0.23,
+    'L': 0.97,
+    'M': 0.74,
+    'N': -0.28,
+    'P': -0.46,
+    'Q': -0.10,
+    'R': -0.14,
+    'S': -0.5,
+    'T': 0.13,
+    'V': 0.76,
+    'W': 0.97,
+    'Y': 0.63,
+    'VOID': -2,
 }
+# reference for the above: see GEM README.md
+
+
+# individual half lives -> divided by 100
+HALF_LIFE = {
+    'A': 0.044,
+    'C': 0.012,
+    'D': 0.011,
+    'E': 0.01,
+    'F': 0.01,
+    'G': 0.30,
+    'H': 0.035,
+    'I': 0.20,
+    'K': 0.013,
+    'L': 0.055,
+    'M': 0.30,
+    'N': 0.014,
+    'P': 0.21,
+    'Q': 0.08,
+    'R': 0.01,
+    'S': 0.019,
+    'T': 0.072,
+    'V': 1,
+    'W': 0.028,
+    'Y': 0.028,
+    'VOID': -1,
+}
+# Reference: Biochemistry: Paul Jay Friedman (Fifth Edition), + biochemistry notes
 
 
 # pseudo molecular fingerprint accounting for biochemical / functional group standout features
@@ -151,4 +158,34 @@ FUNCTIONAL_FP = {
     "S": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
     "T": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
     "P": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    "VOID": [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
 }
+
+
+# [P_alpha, P_beta, P_loops, P_hinge]
+PROPENSITIES = {
+    'A': [1.46, 0.87, 0.71, 1.00],
+    'C': [0.93, 1.37, 0.84, 0.97],
+    'D': [0.96, 0.54, 1.31, 0.87],
+    'E': [1.46, 0.65, 0.85, 0.89],
+    'F': [1.04, 1.44, 0.70, 0.88],
+    'G': [0.42, 0.36, 1.85, 0.96],
+    'H': [0.85, 1.16, 1.02, 0.89],
+    'I': [0.86, 1.85, 0.59, 0.76],
+    'K': [1.12, 0.86, 0.99, 1.24],
+    'L': [1.32, 1.15, 0.65, 0.76],
+    'M': [1.39, 0.76, 0.83, 0.41],
+    'N': [0.75, 0.66, 1.40, 1.21],
+    'P': [0.55, 0.35, 1.75, 1.83],
+    'Q': [1.25, 0.81, 0.92, 1.10],
+    'R': [1.17, 0.88, 0.94, 1.10],
+    'S': [0.68, 0.90, 1.31, 1.41],
+    'T': [0.78, 1.29, 0.99, 1.02],
+    'V': [0.84, 1.90, 0.58, 0.82],
+    'W': [1.13, 1.19, 0.78, 0.86],
+    'Y': [0.98, 1.39, 0.78, 1.02],
+    'VOID': [0, 0, 0, 0],
+}
+
+# Analysis of Domain-Swapped Oligomers Reveals Local Sequence Preferences and Structural Imprints at the Linker Regions and Swapped Interfaces - Scientific Figure on ResearchGate.
+# Available from: https://www.researchgate.net/figure/Propensities-of-amino-acids-to-be-in-alpha-helix-beta-sheets-loops-and-hinge_fig12_230589968 [accessed 1 Jun 2026]
