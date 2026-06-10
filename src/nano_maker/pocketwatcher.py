ef329f3969354eac194b4e5db75b52d41acf4158
Elliot <elliotchan120@gmail.com>
2026-06-10 08:21:51 -0400

    NanoMaker performs core functions. Starting PocketWatcher.

--- src/nano_maker/pocketwatcher.py
+++ src/nano_maker/pocketwatcher.py
@@ -1,18 +1,23 @@
 import torch
 import matplotlib.pyplot as plt
+
 from src.paths import *
+from src.nano_maker.utility import *
 
 class PocketWatcher:
     """visualizes ingested pocket data and can output biochemical and structural data (reports)"""
     def __init__(self):
-        pass
+        self._nanopkt_data = None
+
 
     # pt1 -> get it to retrieve data either by passing the data or the file
     def ingest_file(self, pocket_filepath):
-        pass
+        self._nanopkt_data = load_nano_pocket(pocket_filepath)
+        return True
 
     def ingest_data(self, pocket_data):
-        pass
+        #TODO: data sanity checks before setting pocket_data to class variable
+        self._nanopkt_data = pocket_data
 
 
     # pt2 -> choose between visualizing just the skeleton or the entire pocket
